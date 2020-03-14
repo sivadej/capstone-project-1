1ea5d78f6932b67eccf0fe553c0b84e77f435810
Sivadej K <bombyk@gmail.com>
2020-03-13 18:00:22 -0800

    boilerplate: flask sqlalchemy wtforms debugtoolbar

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+#from forms import FormClassNamesHere
+from models import db, connect_db #, YourModels
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///yourdb'
+app.config['SECRET_KEY'] = 'verysecretindeed'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SQLALCHEMY_ECHO'] = True
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route('/')
+def show_home():
+    return ('hello')
---
+++ models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
