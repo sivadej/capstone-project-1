f439887943596af7ba2c690edd61d0b1824e8954
Sivadej K <bombyk@gmail.com>
2020-03-30 20:05:25 -0800

    style updates to search form

--- app.py
+++ app.py
@@ -33,6 +33,38 @@ def load_user(user_id):
 def temp_home():
     return render_template('newindex.html')
 
+@app.route('/flashme')
+def flashme():
+    flash('redirected to search','info')
+    return redirect('/temp/search')
+
+@app.route('/temp/search', methods=['GET','POST'])
+def temp_search():
+    form = MovieSearchForm()
+    if form.validate_on_submit():
+        # save form data to session, send to results page 1
+        # TODO: use session data for loading search page values from last visit
+        session['audio'] = form.audio.data
+        session['subs'] = form.subs.data
+        session['start_year'] = form.year_from.data
+        session['end_year'] = form.year_to.data
+        session['filter_movie'] = form.filter_movie.data
+        session['filter_series'] = form.filter_series.data
+
+        try:
+            data = json.loads(response)
+            movies = data['results']
+            total = data['total']
+        except:
+            movies = None
+        return redirect('/search/results/1')
+
+    else:
+        if 'total' in session:
+            session.pop('total')
+        return render_template('temp/temp_search.html', form=form)
+    return render_template('temp/temp_search.html')
+
 @app.route('/')
 def show_home():
     print(current_user)
@@ -41,7 +73,14 @@ def show_home():
 @app.route('/logout')
 def logout():
     logout_user()
+    flash('Logged out.','danger')
     return redirect('/')
+
+@app.route('/my_lists')
+@login_required
+def redirect_to_user_watchlists():
+    return redirect(f'/user/{current_user.id}/watchlists')
+
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -52,7 +91,10 @@ def login():
             password = form.password.data,
         )
         login_user(user, remember=form.remember.data)
+        flash('Successfully logged in!','info')
         return redirect('/')
+    #if form.is_submitted():
+    #    flash('Error logging in.','warning')
     return render_template('user/login.html', form=form)
 
 @app.route('/register', methods=['GET','POST'])
