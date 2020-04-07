ae04e90b8bc8ec3ed5398725ce725406faeef7de
Sivadej K <bombyk@gmail.com>
2020-04-06 18:33:09 -0800

    user model tests

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if dev_config is not None:
     from config.app_config import DB_URI, SECRET_KEY
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_ECHO'] = False
 else:
     DB_URI = environ.get('DATABASE_URL')
     SECRET_KEY = environ.get('SECRET_KEY')
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,6 +1,6 @@
 import os
 from unittest import TestCase
-from sqlalchemy import exc
+from sqlalchemy.exc import IntegrityError
 
 from models import db, connect_db, User, Watchlist, SavedMovie, Watchlist_Movie
 
@@ -20,7 +20,67 @@ from app import app
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
 
+class UserModelTests(TestCase):
 
+    def setUp(self):     
+        # empty all database entries
+        db.session.close()
+        db.drop_all()
+        db.create_all()
 
-        
+        # create test users. reassign id numbers
+        test_user_1 = User.register('TestUser1','test1@test.com','password123')
+        test_user_1.id = 1234
 
+        db.session.commit()
+
+        self.client = app.test_client()
+
+    def tearDown(self):
+        resp = super().tearDown()
+        db.session.rollback()
+        return resp
+
+    def test_user_model(self):
+        print('******************************TEST USER MODEL *********')
+        u = User(username='plaintextTest',email='test@test.com',password='password123')
+        self.assertEqual(u.username,'plaintextTest')
+        self.assertEqual(u.email,'test@test.com')
+        self.assertEqual(u.password,'password123')
+        self.assertIsNone(u.id)
+
+    def test_valid_registration(self):
+        print('******************************TEST USER REG *********')
+        # test for proper db insertion and bcrypt hash password
+        # test for plaintext password NOT stored in db
+        u = User.register('test123','test@test.com','password123')
+        db.session.commit()
+        self.assertIsNotNone(u.id)
+        self.assertNotEqual(u.password,'password123')
+        self.assertTrue(u.password.startswith('$2b$'))
+    
+    def test_duplicate_user_reg(self):
+        # Users should not be allowed to register with an already existing name 
+        with self.assertRaises(IntegrityError) as context:
+            dupe = User.register('TestUser1','dupe@email.com','hello321')
+            db.session.commit()
+    
+    def test_invalid_email(self):
+        with self.assertRaises(IntegrityError) as context:
+            invalid = User.register('TestUser500',None,'hello123456')
+            db.session.commit()
+    
+    def test_invalid_passwd(self):
+        with self.assertRaises(ValueError) as context:
+            invalid = User.register('Test2020','email@email.net',None)
+            db.session.commit()
+    
+    def test_user_watchlists(self):
+        # test watchlist relationship in user model
+        u = User.register('iHaveAwatchlist','email@gmail.cn','password-o-clock')
+        u.id = 99999
+        db.session.add(Watchlist(title='Test Watchlist',is_shared=False, user_id=u.id))
+        db.session.commit()
+        print(u.watchlists)
+        self.assertEqual(u.watchlists[0].user_id,u.id)
+        self.assertEqual(len(u.watchlists),1)
